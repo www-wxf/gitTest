112071e1a70994e4bb9b904dc9d4302eca3c29bc
wxf <1835732076@qq.com>
2020-01-02 09:57:21 +0800

    update User

--- src/main/java/com/itmy/User.java
+++ src/main/java/com/itmy/User.java
@@ -5,6 +5,15 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String name;
     private String age;
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     public String getName() {
         return name;
